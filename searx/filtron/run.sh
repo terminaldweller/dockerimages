fc68d6e6968a5c681bbc4684ecee15f2cecefe81
terminaldweller <thabogre@gmail.com>
2021-09-02 13:07:27 +0430

    filtron and morty arm builders

---
+++ searx/filtron/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+set -e
+set -x
+
+docker buildx create armbuilder
+docker buildx use armbuilder
+docker buildx build --push --platform linux/arm/v7,linux/arm/v8,linux/amd64 --tag bloodstalker/filtron .
